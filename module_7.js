f7cfe542e184f2be72d2eb9f2883fc41681fe651
Mounieshh <iammounieshv@gmail.com>
2026-01-28 13:09:51 +0530

    this keyword

--- module_7.js
+++ module_7.js
@@ -36,6 +36,86 @@ console.log(add9(80));
 
 // Hoisting -> moving on top the declarations during compilation process
 
+//Function Hoisting
+
+addSum(2,7)
+
+function addSum(a, b){
+    console.log(a + b);
+}
+
+
+//variable hoisting 
+
+console.log(x2);
+var x2 = 23
+console.log(x2);
+
+
+function variableHos(){
+    console.log(ret);
+
+    var ret = 89
+    console.log(ret);
+
+    function functionHost(){
+        return ret + ret
+    }
+
+    return functionHost
+    
+}
+const funcHost = variableHos()
+console.log(funcHost());
+
+
+// this keyword 
+
+console.log(this);
+
+const name = "Ryzen"
+const obj = {
+    name : "Jake",
+    greet() {
+        console.log(this.name);
+    }
+}
+
+obj.greet()
+
+
+// "this" in functions #2
+
+function printThis(){
+    console.log(this); // window object
+}
+
+// printThis()
+
+"use strict"
+function showPrintThis(){
+    console.log(this);
+}
+
+// showPrintThis()
+
+// "this" in arrow functions #3 (only inherited from the parent)
+
+const variableThis = {
+    name : "Mouniesh",
+    age : 21,
+
+    greet() {
+        console.log("This is regular function ", this.name);
+    },
+
+    arrowFunc : () => {
+        console.log("Arrow:", this.name, " and the age is ", this.age);
+    }
+}
+
+variableThis.arrowFunc()
+variableThis.greet()
 
 
 
